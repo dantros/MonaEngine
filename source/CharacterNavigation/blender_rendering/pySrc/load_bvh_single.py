54a73849147866b1aacabf32ed90af517cb52edb
Agustin <agumatthey@gmail.com>
2022-04-30 10:48:18 -0400

    se agregan archivos py necesarios para blender_rendering

--- source/CharacterNavigation/blender_rendering/pySrc/load_bvh_single.py
+++ source/CharacterNavigation/blender_rendering/pySrc/load_bvh_single.py
@@ -1,14 +1,11 @@
 import os
 import sys
-sys.path.append(os.getcwd())
-sys.path.append('../')
-print(sys.path)
+sys.path.append(os.getcwd() + "/pySrc")
 
-from utils import BVH
+import BVH
 import numpy as np
 import bpy
 import mathutils
-import math
 import pdb
 
 #scale factor for bone length
@@ -105,7 +102,7 @@ def add_joint(location, parent_obj, name):
 
     return new_joint
 
-def build_t_pose(file: BVH_file, joint, parent_obj, all_obj, rootOffset):
+def build_t_pose(file: BVH_file, joint, parent_obj, all_obj):
     if joint != 0:
         offset = mathutils.Vector(file.offsets[joint])
         new_bone = add_bone(offset, parent_obj, file.names[joint] + '_bone')
@@ -113,12 +110,12 @@ def build_t_pose(file: BVH_file, joint, parent_obj, all_obj, rootOffset):
         all_obj.append(new_bone)
         all_obj.append(new_joint)
     else:
-        new_joint = add_joint(rootOffset, None, file.names[joint] + '_end')
+        new_joint = add_joint(mathutils.Vector((0., 0., 0.)), None, file.names[joint] + '_end')
         all_obj.append(new_joint)
 
     for i in range(len(file.topology)):
         if file.topology[i] == joint:
-            build_t_pose(file, i, new_joint, all_obj, rootOffset)
+            build_t_pose(file, i, new_joint, all_obj)
 
 
 def set_parent(parent, child):
@@ -129,97 +126,53 @@ def set_parent(parent, child):
     '''
 
 
-def set_animation(inputFile, resultFile, joints, resultOffset):
+def set_animation(file, joints):
     bpy.context.scene.frame_start = 0
-    bpy.context.scene.frame_end = inputFile.anim.rotations.shape[0] - 1
+    bpy.context.scene.frame_end = file.anim.rotations.shape[0] - 1
 
-    bpy.context.scene.render.fps = 1 / inputFile.frametime
+    bpy.context.scene.render.fps = 1 / file.frametime
 
     bpy.ops.object.select_all(action='DESELECT')
 
     print('Set fps to', bpy.context.scene.render.fps)
-    print(inputFile.frame_num, 'frames in total')
-    resFactor = int(round((inputFile.frame_num / resultFile.frame_num), 0))
-    print('factor: ' + str(resFactor))
-    for frame in range(0, inputFile.frame_num):
-        joints[0].location = inputFile.anim.positions[frame, 0, :]
+    print(file.frame_num, 'frames in total')
+
+    for frame in range(0, file.frame_num):
+        joints[0].location = file.anim.positions[frame, 0, :]
         joints[0].keyframe_insert(data_path='location', frame=frame)
         if frame % 100 == 99:
-            print('[{}/{}] done.'.format(frame+1, inputFile.frame_num))
-        for j in range(inputFile.joint_num):
+            print('[{}/{}] done.'.format(frame+1, file.frame_num))
+        for j in range(file.joint_num):
             joints[j].rotation_mode = 'QUATERNION'
-            joints[j].rotation_quaternion = mathutils.Quaternion(inputFile.anim.rotations.qs[frame, j, :])
+            joints[j].rotation_quaternion = mathutils.Quaternion(file.anim.rotations.qs[frame, j, :])
             joints[j].keyframe_insert(data_path='rotation_quaternion', frame=frame)
-        
-        if frame >= resultFile.frame_num:
-            continue
-        frame_amp = frame*resFactor
-        for f in range(resFactor):   
-            joints[inputFile.joint_num].location = resultFile.anim.positions[frame, 0, :] + resultOffset
-            joints[inputFile.joint_num].keyframe_insert(data_path='location', frame=frame_amp+f)
-        if frame % 100 == 99:
-            print('[{}/{}] done.'.format(frame+1, inputFile.frame_num))
-        for j in range(resultFile.joint_num):
-            for f in range(resFactor):   
-                joints[inputFile.joint_num + j].rotation_mode = 'QUATERNION'
-                joints[inputFile.joint_num + j].rotation_quaternion = mathutils.Quaternion(resultFile.anim.rotations.qs[frame, j, :])
-                joints[inputFile.joint_num + j].keyframe_insert(data_path='rotation_quaternion', frame=frame_amp+f)
 
     bpy.context.scene.frame_current = 0
 
 
-def load_bvh(input, result):
+def load_bvh(file_name):
     print('Loading BVH file......')
-    inputFile = BVH_file(input)
-    resultFile = BVH_file(result)
+    file = BVH_file(file_name)
     print('Loading BVH file done.')
 
     print('Building T-Pose......')
-
-    for n, name in enumerate(inputFile.names):
-        inputFile.names[n] = 'input_' + name
-
-    for n, name in enumerate(resultFile.names):
-        resultFile.names[n] = 'result_' + name
-    # input
-    all_obj1 = []
-    rootOffset = mathutils.Vector((0., 0., 0.))
-    build_t_pose(inputFile, 0, None, all_obj1, rootOffset)
-
-    # result
-    all_obj2 = []
-    rootOffset = mathutils.Vector((0., 15., 0.))
-    build_t_pose(resultFile, 0, None, all_obj2, rootOffset)
-
+    all_obj = []
+    build_t_pose(file, 0, None, all_obj)
     print('Building T-Pose done.')
 
     print('Loading keyframes......')
 
     #pairing object order and file.animation's order
-    all_obj = all_obj1 + all_obj2 #concat
     all_joints = []
-    for j in range(inputFile.joint_num):
-        name = inputFile.names[j]
-        for obj in all_obj1:
+    for j in range(file.joint_num):
+        name = file.names[j]
+        for obj in all_obj:
             if obj.name == name + '_end':
                 all_joints.append(obj)
                 break
-    for j in range(resultFile.joint_num):
-        name = resultFile.names[j]
-        for obj in all_obj2:
-            if obj.name == name + '_end':
-                all_joints.append(obj)
-                break
-    
-    resultOffset = [0, 15, 0]
-    set_animation(inputFile, resultFile, all_joints, resultOffset)
-
-    mat = bpy.data.materials.new("PKHG")
-    mat.diffuse_color = (float(.5),1.0,0.0, 1.0)
-    for obj in all_obj2:
-        obj.active_material = mat
-    
+    set_animation(file, all_joints)
     print('Loading keyframes done.')
+
     bpy.ops.object.select_all(action='DESELECT')
     for obj in all_obj:
         obj.select_set(True)
@@ -235,6 +188,5 @@ if __name__ == '__main__':
     bpy.ops.object.delete()
 
     input = [f for f in os.listdir('./') if 'input.bvh' in f][0]
-    result = [f for f in os.listdir('./') if 'result.bvh' in f][0]
-
-    load_bvh(input, result)
+    
+    load_bvh(input)
--- tests/test.cpp
+++ tests/test.cpp
@@ -9,9 +9,9 @@ int main(){
     std::string file_path = __FILE__;
     std::string dir_path = file_path.substr(0, file_path.rfind("\\"));
     std::replace(dir_path.begin(), dir_path.end(), '\\', '/');
-    Mona::BVH_file file1 = bvhManager->readBVH(dir_path + std::string("/BigVegas.bvh"));
-    std::string staticPath = dir_path + std::string("/BigVegas.bvh");
-    std::string writePath = dir_path + std::string("/BigVegasCopy1.bvh");
+    Mona::BVH_file file1 = bvhManager->readBVH(dir_path + std::string("/input.bvh"));
+    std::string staticPath = dir_path + std::string("/input.bvh");
+    std::string writePath = dir_path + std::string("/inputCopy.bvh");
     bvhManager->writeBVH(file1.m_rotations, file1.m_rootPositions, file1.m_frametime, file1.m_frameNum, staticPath, writePath);
     Mona::BVHManager::DestroyInstance();
     while (true) {

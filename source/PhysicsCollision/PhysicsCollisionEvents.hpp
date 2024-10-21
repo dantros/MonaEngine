92e141d1e007be9998a062312a7952bd29713b62
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2024-10-21 01:26:03 -0300

    formatting source PhysicsCollision

--- source/PhysicsCollision/PhysicsCollisionEvents.hpp
+++ source/PhysicsCollision/PhysicsCollisionEvents.hpp
@@ -3,34 +3,35 @@
 #define PHYSICSCOLLISIONEVENTS_HPP
 #include "../Event/Events.hpp"
 #include "../World/ComponentHandle.hpp"
-#include "RigidBodyComponent.hpp"
 #include "CollisionInformation.hpp"
+#include "RigidBodyComponent.hpp"
 namespace Mona {
 
-	struct StartCollisionEvent : public Event {
-		static constexpr uint8_t eventIndex = GetEventIndex(EEventType::StartCollisionEvent);
-		StartCollisionEvent(RigidBodyHandle&rb0, RigidBodyHandle&rb1, bool swaped, CollisionInformation& info) :
-			firstRigidBody(rb0),
-			secondRigidBody(rb1),
-			areSwaped(swaped),
-			collisionInfo(info)
-		{}
-		RigidBodyHandle& firstRigidBody;
-		RigidBodyHandle& secondRigidBody;
-		bool areSwaped;
-		CollisionInformation& collisionInfo;
+struct StartCollisionEvent : public Event {
+    static constexpr uint8_t eventIndex = GetEventIndex(EEventType::StartCollisionEvent);
+    StartCollisionEvent(RigidBodyHandle& rb0, RigidBodyHandle& rb1, bool swaped, CollisionInformation& info)
+        : firstRigidBody(rb0)
+        , secondRigidBody(rb1)
+        , areSwaped(swaped)
+        , collisionInfo(info)
+    {
+    }
+    RigidBodyHandle& firstRigidBody;
+    RigidBodyHandle& secondRigidBody;
+    bool areSwaped;
+    CollisionInformation& collisionInfo;
+};
 
-	};
+struct EndCollisionEvent : public Event {
+    static constexpr uint8_t eventIndex = GetEventIndex(EEventType::EndCollisionEvent);
+    EndCollisionEvent(RigidBodyHandle& rb0, RigidBodyHandle& rb1)
+        : firstRigidBody(rb0)
+        , secondRigidBody(rb1)
+    {
+    }
+    RigidBodyHandle& firstRigidBody;
+    RigidBodyHandle& secondRigidBody;
+};
 
-	struct EndCollisionEvent : public Event {
-		static constexpr uint8_t eventIndex = GetEventIndex(EEventType::EndCollisionEvent);
-		EndCollisionEvent(RigidBodyHandle& rb0, RigidBodyHandle& rb1) :
-			firstRigidBody(rb0),
-			secondRigidBody(rb1)
-		{}
-		RigidBodyHandle& firstRigidBody;
-		RigidBodyHandle& secondRigidBody;
-	};
-	
 }
 #endif

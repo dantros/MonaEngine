53210857d14e59d9a616fac64dd45e9ca937adbf
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2024-10-21 01:14:05 -0300

    formating examples

--- examples/MinimalSetup.cpp
+++ examples/MinimalSetup.cpp
@@ -1,21 +1,22 @@
 #include "MonaEngine.hpp"
-class MinimalSetup : public Mona::Application
-{
+class MinimalSetup : public Mona::Application {
 public:
-	MinimalSetup() = default;
-	~MinimalSetup() = default;
-	virtual void UserStartUp(Mona::World &world) noexcept override{
-	}
+    MinimalSetup() = default;
+    ~MinimalSetup() = default;
+    virtual void UserStartUp(Mona::World& world) noexcept override
+    {
+    }
 
-	virtual void UserShutDown(Mona::World& world) noexcept override {
-	}
-	virtual void UserUpdate(Mona::World& world, float timeStep) noexcept override {
-	}
+    virtual void UserShutDown(Mona::World& world) noexcept override
+    {
+    }
+    virtual void UserUpdate(Mona::World& world, float timeStep) noexcept override
+    {
+    }
 };
 int main()
-{	
-	MinimalSetup app;
-	Mona::Engine engine(app);
-	engine.StartMainLoop();
-	
+{
+    MinimalSetup app;
+    Mona::Engine engine(app);
+    engine.StartMainLoop();
 }

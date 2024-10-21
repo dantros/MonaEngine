8014a35ba04817fbe067ac3866da5ec36ec4babb
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2024-10-21 01:54:31 -0300

    formatting source World

--- source/World/ComponentTypes.hpp
+++ source/World/ComponentTypes.hpp
@@ -1,61 +1,62 @@
 #pragma once
 #ifndef COMPONENTTYPES_HPP
 #define COMPONENTTYPES_HPP
-#include <cstdint>
 #include "../Core/Common.hpp"
+#include <cstdint>
 namespace Mona {
-	enum class EComponentType : uint8_t {
-		TransformComponent,
-		CameraComponent,
-		StaticMeshComponent,
-		RigidBodyComponent,
-		AudioSourceComponent,
-		DirectionalLightComponent,
-		SpotLightComponent,
-		PointLightComponent,
-		SkeletalMeshComponent,
-		IKNavigationComponent,
-		ComponentTypeCount
-	};
+enum class EComponentType : uint8_t {
+    TransformComponent,
+    CameraComponent,
+    StaticMeshComponent,
+    RigidBodyComponent,
+    AudioSourceComponent,
+    DirectionalLightComponent,
+    SpotLightComponent,
+    PointLightComponent,
+    SkeletalMeshComponent,
+    IKNavigationComponent,
+    ComponentTypeCount
+};
 
-	constexpr uint8_t GetComponentIndex(EComponentType type) {
-		return static_cast<uint8_t>(type);
-	}
-	constexpr uint8_t GetComponentTypeCount()
-	{
-		return static_cast<uint8_t>(EComponentType::ComponentTypeCount);
-	}
-	template <typename ...ComponentTypes> struct DependencyList {};
+constexpr uint8_t GetComponentIndex(EComponentType type)
+{
+    return static_cast<uint8_t>(type);
+}
+constexpr uint8_t GetComponentTypeCount()
+{
+    return static_cast<uint8_t>(EComponentType::ComponentTypeCount);
+}
+template <typename... ComponentTypes>
+struct DependencyList { };
 
-	class TransformComponent;
-	class StaticMeshComponent;
-	class CameraComponent;
-	class RigidBodyComponent;
-	class AudioSourceComponent;
-	class DirectionalLightComponent;
-	class SpotLightComponent;
-	class PointLightComponent;
-	class SkeletalMeshComponent;
-	class IKNavigationComponent;
-	template <typename ComponentType>
-	class DefaultLifetimePolicy;
+class TransformComponent;
+class StaticMeshComponent;
+class CameraComponent;
+class RigidBodyComponent;
+class AudioSourceComponent;
+class DirectionalLightComponent;
+class SpotLightComponent;
+class PointLightComponent;
+class SkeletalMeshComponent;
+class IKNavigationComponent;
+template <typename ComponentType>
+class DefaultLifetimePolicy;
 
+template <typename ComponentType>
+class ComponentManager;
 
-	template <typename ComponentType>
-		class ComponentManager;
-
-	template <typename ComponentType>
-	inline constexpr bool is_component = is_any<ComponentType,
-		TransformComponent,
-		CameraComponent,
-		StaticMeshComponent,
-		RigidBodyComponent,
-		AudioSourceComponent,
-		DirectionalLightComponent,
-		SpotLightComponent,
-		PointLightComponent,
-		SkeletalMeshComponent,
-		IKNavigationComponent>;
+template <typename ComponentType>
+inline constexpr bool is_component = is_any<ComponentType,
+    TransformComponent,
+    CameraComponent,
+    StaticMeshComponent,
+    RigidBodyComponent,
+    AudioSourceComponent,
+    DirectionalLightComponent,
+    SpotLightComponent,
+    PointLightComponent,
+    SkeletalMeshComponent,
+    IKNavigationComponent>;
 
 }
 #endif
--- source/World/GameObject.hpp
+++ source/World/GameObject.hpp
@@ -1,80 +1,92 @@
 #pragma once
 #ifndef GAMEOBJECT_HPP
 #define GAMEOBJECT_HPP
+#include "ComponentTypes.hpp"
+#include "GameObjectTypes.hpp"
 #include <limits>
 #include <unordered_map>
-#include "GameObjectTypes.hpp"
-#include "ComponentTypes.hpp"
 namespace Mona {
-	class World;
-	class GameObjectManager;
-	class GameObject {
-	public:
-		enum class EState {
-			Active,
-			PendingDestroy
-		};
-		
-		virtual ~GameObject() {};
-		
-		
-		void StartUp(World& world) noexcept 
-		{ 
-			UserStartUp(world);
-		};
+class World;
+class GameObjectManager;
+class GameObject {
+public:
+    enum class EState {
+        Active,
+        PendingDestroy
+    };
 
-		void Update(World& world, float timeStep) noexcept {
-			if (m_state == EState::PendingDestroy)
-				return;
-			UserUpdate(world, timeStep);
-		}
+    virtual ~GameObject() {};
 
-		virtual void UserUpdate(World& world, float timeStep) noexcept {};
-		virtual void UserStartUp(World& world) noexcept {};
+    void StartUp(World& world) noexcept
+    {
+        UserStartUp(world);
+    };
 
-		const EState GetState() const { return m_state; }
-		template <typename ComponentType>
-		bool HasComponent() const {
-			static_assert(is_component<ComponentType>, "Template parameter is not a component");
-			auto it = m_componentHandles.find(ComponentType::componentIndex);
-			return it != m_componentHandles.end();
-		}
-		InnerGameObjectHandle GetInnerObjectHandle() const noexcept { return m_objectHandle; }
-		template <typename ComponentType>
-		InnerComponentHandle GetInnerComponentHandle() const {
-			static_assert(is_component<ComponentType>, "Template parameter is not a component");
-			auto it = m_componentHandles.find(ComponentType::componentIndex);
-			if (it != m_componentHandles.end())
-				return it->second;
-			else return InnerComponentHandle();
-		}
-	protected:
-		GameObject(const GameObject&) = delete;
-		GameObject& operator=(const GameObject&) = delete;
-		GameObject(GameObject&&) = default;
-		GameObject& operator=(GameObject&&) = default;
-		GameObject() : m_objectHandle(), m_state(EState::Active) {}
-	private:
-		
-		friend class GameObjectManager;
-		friend class World;
-		void ShutDown() noexcept {
-			m_state = EState::PendingDestroy;
-		}
-		void SetObjectHandle(const InnerGameObjectHandle& handle) {
-			m_objectHandle = handle;
-		}
+    void Update(World& world, float timeStep) noexcept
+    {
+        if (m_state == EState::PendingDestroy)
+            return;
+        UserUpdate(world, timeStep);
+    }
 
-		void RemoveInnerComponentHandle(decltype(GetComponentTypeCount()) componentIndex){
-			m_componentHandles.erase(componentIndex);
-		}
+    virtual void UserUpdate(World& world, float timeStep) noexcept {};
+    virtual void UserStartUp(World& world) noexcept {};
 
-		void AddInnerComponentHandle(decltype(GetComponentTypeCount()) componentIndex, InnerComponentHandle componentHandle) {
-			m_componentHandles[componentIndex] = componentHandle;
-		}
-		InnerGameObjectHandle m_objectHandle;
-		EState m_state;
-		std::unordered_map<decltype(GetComponentTypeCount()), InnerComponentHandle> m_componentHandles;
-	};
+    const EState GetState() const { return m_state; }
+    template <typename ComponentType>
+    bool HasComponent() const
+    {
+        static_assert(is_component<ComponentType>, "Template parameter is not a component");
+        auto it = m_componentHandles.find(ComponentType::componentIndex);
+        return it != m_componentHandles.end();
+    }
+    InnerGameObjectHandle GetInnerObjectHandle() const noexcept { return m_objectHandle; }
+    template <typename ComponentType>
+    InnerComponentHandle GetInnerComponentHandle() const
+    {
+        static_assert(is_component<ComponentType>, "Template parameter is not a component");
+        auto it = m_componentHandles.find(ComponentType::componentIndex);
+        if (it != m_componentHandles.end())
+            return it->second;
+        else
+            return InnerComponentHandle();
+    }
+
+protected:
+    GameObject(const GameObject&) = delete;
+    GameObject& operator=(const GameObject&) = delete;
+    GameObject(GameObject&&) = default;
+    GameObject& operator=(GameObject&&) = default;
+    GameObject()
+        : m_objectHandle()
+        , m_state(EState::Active)
+    {
+    }
+
+private:
+    friend class GameObjectManager;
+    friend class World;
+    void ShutDown() noexcept
+    {
+        m_state = EState::PendingDestroy;
+    }
+    void SetObjectHandle(const InnerGameObjectHandle& handle)
+    {
+        m_objectHandle = handle;
+    }
+
+    void RemoveInnerComponentHandle(decltype(GetComponentTypeCount()) componentIndex)
+    {
+        m_componentHandles.erase(componentIndex);
+    }
+
+    void AddInnerComponentHandle(decltype(GetComponentTypeCount()) componentIndex, InnerComponentHandle componentHandle)
+    {
+        m_componentHandles[componentIndex] = componentHandle;
+    }
+    InnerGameObjectHandle m_objectHandle;
+    EState m_state;
+    std::unordered_map<decltype(GetComponentTypeCount()), InnerComponentHandle> m_componentHandles;
+};
 }
 #endif
